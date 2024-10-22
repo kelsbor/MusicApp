bd0d142213418aac055919dd2e326088a210f2b7
Kehsbor <kehsbor@gmail.com>
2024-10-22 18:59:33 -0300

    Project Creation and update README

---
+++ music-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Piano from './components/PianoKeyboard.js'
+
+function App() {
+  return (
+    <>
+      <Piano />
+      <h1>Hello!</h1>
+    </>
+  );
+}
+
+export default App;
